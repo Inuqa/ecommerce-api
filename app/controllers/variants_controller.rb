9fd813bc97c6904870e12960fd1c4a15b8bf1afd
inuqa <seba.nrc@gmail.com>
2021-05-06 22:31:22 -0400

    added products and variants controller

---
+++ app/controllers/variants_controller.rb
@@ -0,0 +1,13 @@
+class VariantsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'variants/index'
+  get 'variants/create'
+  get 'variants/update'
+  get 'variants/destroy'
+  get 'products/index'
+  get 'products/create'
+  get 'products/update'
+  get 'products/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
