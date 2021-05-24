5c84ef907894f7a40e94726861aab41033e7597f
inuqa <seba.nrc@gmail.com>
2021-05-24 00:36:29 -0400

    added valiadtions and methods to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,20 @@
 class Order < ApplicationRecord
+  enum status: { pendiente: 0, pagado: 1, enviado: 2, cancelado: 3 }
   belongs_to :user
+  has_many :line_items, dependent: :destroy
+
+  scope :filter_by_status, ->(status) { where status: status }
+
+  scope :filter_by_email, ->(email) { joins(:user).where("user.email": email) }
+
+  scope :filter_by_id, ->(id) { where id: id }
+
+  def payed_mail
+    @order = Order.find(id)
+    OrderMailer.with(order: @order).order_payed.deliver_later
+  end
+
+  def status_payed?
+    status == 'pagado'
+  end
 end
