0dd596003f7cd2e4c7b3e16b02c4106ec110b1f6
inuqa <seba.nrc@gmail.com>
2021-05-24 18:01:22 -0400

    added configuration for cookie based authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+
+  helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!, :set_user
+
+  def login!
+    session[:admin_id] = @admin_id
+  end
+
+  def logged_in?
+    !!session[:admin_id]
+  end
+
+  def current_user
+    @current_user ||= Admin.find(session[:admin_id]) if session[:admin_id]
+  end
+
+  def authorized_user?
+    @admin == current_user
+  end
+
+  def logout!
+    session.clear
+  end
+
+  def set_user
+    @admin = Admin.find_by(id: session[:admin_id])
+  end
 end
