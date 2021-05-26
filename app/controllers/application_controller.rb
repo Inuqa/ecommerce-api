7e431e10d7af200daf6f38d628361bbe63fcb02a
inuqa <seba.nrc@gmail.com>
2021-05-26 04:45:54 -0400

    refactor code in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,15 @@ class ApplicationController < ActionController::Base
   helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!, :set_user
 
   def login!
-    session[:admin_id] = @admin_id
+    session[:admin] = @admin
   end
 
   def logged_in?
-    !!session[:admin_id]
+    !!session[:admin]
   end
 
   def current_user
-    @current_user ||= Admin.find(session[:admin_id]) if session[:admin_id]
+    @current_user ||= Admin.find(session[:admin]['id']) if session[:admin]
   end
 
   def authorized_user?
@@ -24,6 +24,10 @@ class ApplicationController < ActionController::Base
   end
 
   def set_user
-    @admin = Admin.find_by(id: session[:admin_id])
+    @admin = Admin.find_by(id: session[:admin].id)
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
   end
 end
