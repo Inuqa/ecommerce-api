4b4ad9eee6f5177c4b0fd5453f041e42ca7a3a15
inuqa <seba.nrc@gmail.com>
2021-05-26 04:47:19 -0400

    fixed a bug in session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-if Rails.env === 'production'
+if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store, key: '_api', domain: '_coyote_api'
 else
   Rails.application.config.session_store :cookie_store, key: '_api'
