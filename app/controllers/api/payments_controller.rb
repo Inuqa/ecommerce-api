e16dc29e8ff2395570d32aa6d5dbc549e8301f16
inuqa <seba.nrc@gmail.com>
2021-05-31 03:36:35 -0400

    added some code to payments commit action

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -1,8 +1,17 @@
 class Api::PaymentsController < ApplicationController
   def commit
     @token = params[:token_ws]
+    order = Order.find_by(token: @token)
     res = Transbank::Webpay::WebpayPlus::Transaction.commit(token: @token)
 
-    redirect_to 'http://localhost:3000/products'
+    if res.status == 'AUTHORIZED'
+      order.status = 'pagado'
+      order.save
+    end
+    redirect_to "http://localhost:3000/transactions/#{order.id}"
+  end
+
+  def show
+    @order = Order.find(params[:id])
   end
 end
