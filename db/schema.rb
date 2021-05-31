53f4a28ea61158b06013e922eecb8916d2a0e20d
inuqa <seba.nrc@gmail.com>
2021-05-31 03:46:46 -0400

    added discarded_at to variants model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_28_140702) do
+ActiveRecord::Schema.define(version: 2021_05_31_074554) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -95,6 +95,8 @@ ActiveRecord::Schema.define(version: 2021_05_28_140702) do
     t.integer "product_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "discarded_at"
+    t.index ["discarded_at"], name: "index_variants_on_discarded_at"
     t.index ["product_id"], name: "index_variants_on_product_id"
   end
 
