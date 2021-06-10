8771bf57234cb19daa49451b9093c59aa42ea3be
inuqa <seba.nrc@gmail.com>
2021-06-09 20:19:00 -0400

    added pay_method column to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_111827) do
+ActiveRecord::Schema.define(version: 2021_06_10_001822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_06_03_111827) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "token"
     t.integer "amount", null: false
+    t.string "pay_method"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
