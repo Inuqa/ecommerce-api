f1dfa129689beed9f2946da7908b5154ce802190
inuqa <seba.nrc@gmail.com>
2021-06-09 20:22:58 -0400

    added shipping_method to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_001822) do
+ActiveRecord::Schema.define(version: 2021_06_10_001936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2021_06_10_001822) do
     t.string "token"
     t.integer "amount", null: false
     t.string "pay_method"
+    t.string "shipping_method"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
