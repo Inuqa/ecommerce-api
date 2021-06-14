3b82f581345c7a1bf1c345690ced7c79b178a1f1
Inuka <seba.nrc@gmail.com>
2021-06-14 16:07:09 -0400

    added a set order before action and show and update actions

--- app/controllers/api/admin/orders_controller.rb
+++ app/controllers/api/admin/orders_controller.rb
@@ -1,5 +1,6 @@
 class Api::Admin::OrdersController < ApplicationController
   before_action :authorized
+  before_action :set_order, only: %i[show update]
 
   def index
     @orders = Order.all
@@ -10,7 +11,22 @@ class Api::Admin::OrdersController < ApplicationController
     @orders = @orders.limit(10)
   end
 
+  def show; end
+
+  def update
+    @order.update(order_params)
+    render json: @order
+  end
+
   private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
 
   def filter(orders)
     orders = orders.filter_by_id(params[:order_id]) if params[:order_id].present?
