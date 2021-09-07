fae5ee59c1287e5357667f242206e13ca6f62bf1
Sebastian Rojas <srojas@nnodes.com>
2021-09-07 19:43:10 -0300

    added property model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_07_010225) do
+ActiveRecord::Schema.define(version: 2021_09_07_224118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -105,6 +105,12 @@ ActiveRecord::Schema.define(version: 2021_09_07_010225) do
     t.text "description"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["discarded_at"], name: "index_products_on_discarded_at"
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
